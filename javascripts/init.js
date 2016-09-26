20590aafd85056adaa87b49bb826d9c58045043e
Aravind Venkat <arvindwidu@gmail.com>
2016-09-26 01:54:28 -0400

    ch5
    validations updated

--- javascripts/init.js
+++ javascripts/init.js
@@ -105,19 +105,6 @@
 /*	Modal Popup
 ------------------------------------------------------*/
 
-
-  /*  $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });*/
-
-
-
     $(document).on('click', '.popup-modal-dismiss', function (e) {
     		e.preventDefault();
     		$.magnificPopup.close();
@@ -148,60 +135,66 @@
    $('form#contactForm button.submit').click(function() {
 
       $('#image-loader').fadeIn();
+      $('#message-warning').fadeOut();
 
       var contactName = $('#contactForm #contactName').val();
       var contactEmail = $('#contactForm #contactEmail').val();
       var contactSubject = $('#contactForm #contactSubject').val();
       var contactMessage = $('#contactForm #contactMessage').val();
 
+if(contactName == "" || contactEmail=="" || contactMessage == "")
+{
+  msg = "Please Enter all mandatory fields."
+  $('#image-loader').fadeOut();
+  $('#message-warning').html(msg);
+  $('#message-warning').fadeIn();
+  $('#message-warning').focus();
+}
+else if(!isValidEmailAddress( contactEmail ))
+{
+  msg = "Please Enter valid email."
+  $('#image-loader').fadeOut();
+  $('#message-warning').html(msg);
+  $('#message-warning').fadeIn();
+  $('#message-warning').focus();
+}
+else {
+  console.log({"name":contactName,"email":contactEmail,"subject":contactSubject,"message":contactMessage});
+      emailjs.send("gmail", "avenkit_services",
+          {"name":contactName,"email":contactEmail,"subject":contactSubject,"message":contactMessage})
+      .then(function(response) {
+         console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+         $('#image-loader').fadeOut();
+         $('#message-warning').hide();
+         $('#contactForm').fadeOut();
+         $('#message-success').fadeIn();
+      }, function(err) {
+         console.log("FAILED. error=", err);
+         $('#image-loader').fadeOut();
+         $('#message-warning').html(msg);
+        $('#message-warning').fadeIn();
+      });
 
-console.log({"name":contactName,"email":contactEmail,"subject":contactSubject,"message":contactMessage});
-    emailjs.send("gmail", "avenkit_services",
-        {"name":contactName,"email":contactEmail,"subject":contactSubject,"message":contactMessage})
-    .then(function(response) {
-       console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-       $('#image-loader').fadeOut();
-       $('#message-warning').hide();
-       $('#contactForm').fadeOut();
-       $('#message-success').fadeIn();
-    }, function(err) {
-       console.log("FAILED. error=", err);
-       $('#image-loader').fadeOut();
-       $('#message-warning').html(msg);
-      $('#message-warning').fadeIn();
-    });
+    }
 
-      /*
-      'subject=' + contactSubject + 'message=' + contactMessage;
+    function isValidEmailAddress(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
+    };
 
-
-      $.ajax({
-      var data = 'name=' + contactName + 'email=' + contactEmail +
-	      type: "POST",
-	      url: "/",
-        contentType: 'application/json',
-	      data: JSON.stringify({data}),
-	      success: function(err,res) {
-        console.log(res);
-            // Message was sent
-            if (res = 'success') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });*/
       return false;
    });
 
+
+
+   $('.item-wrap a').magnificPopup({
+
+       type:'inline',
+       fixedContentPos: false,
+       removalDelay: 200,
+       showCloseBtn: false,
+       mainClass: 'mfp-fade'
+
+    });
 
 });
