b094f328facff809af1037c14c494c327ec6cf9f
Aravind <aravind@Aravinds-MacBook-Pro.local>
2018-03-12 12:45:17 -0400

    welcome page flex update.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,10 @@
 
 
-
-
-//refreshContact();
-
+refreshContact();
 
 function openTab(event, tabID) {
     document.getElementById(tabID).click();
 }
-
-
-/*function openTab(event, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
-}*/
 
 function openURL(event, link) {
     location.href = link;
